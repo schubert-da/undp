c3c354fda05c59789be39b6cf04273f666022e73
schubert <schubertdeabreu2@gmail.com>
2022-10-12 15:28:28 +0530

    data changes. changes to sourceGraph. sourceCountry added

---
+++ src/components/sourcesGraph/sourceCountry/sourceCountry.svelte
@@ -0,0 +1,96 @@
+<script>
+    export let country;
+
+</script>
+
+<div class="country-container">
+    <div class="title">
+        {country.country}
+    </div>
+    <div class="rect-container">
+        {#each  Array(country["Household application"]) as _, i}
+            <div class="rect rect-household"></div>
+        {/each}
+
+        {#each  Array(country["Solar"]) as _, i}
+            <div class="rect rect-solar"></div>
+        {/each}
+
+        {#each  Array(country["Thermal"]) as _, i}
+            <div class="rect rect-thermal"></div>
+        {/each}
+
+        {#each  Array(country["Renewable general"]) as _, i}
+            <div class="rect rect-renewable"></div>
+        {/each}
+
+        {#each  Array(country["Other"]) as _, i}
+            <div class="rect rect-other"></div>
+        {/each}
+
+    </div>
+</div>
+
+<style>
+
+    .country-container{
+        margin-bottom: 20px;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+
+        border: 1px solid #999;
+        border-radius: 5px;
+        max-width: 250px;
+
+        position: relative;
+    }
+
+    .title{
+        padding: 0px 5px;
+        font-family: 'Roboto';
+        font-weight: bold;
+        background-color: #fff;
+
+        position: absolute;
+        left: 15px;
+        top: -12px;
+    }
+    
+
+    .rect-container{
+        display: flex;
+        flex-wrap: wrap;
+        width: 220px;
+        margin: 12px 10px 10px 10px;
+    }
+
+    .rect{
+        width: 30px;
+        height: 30px;
+        margin: 2px;
+
+        background-color: #666;
+        border: 1px solid #222;
+        border-radius: 2px;
+    }
+
+    .rect-household{
+        background-color: #509DBA;
+    }
+
+    .rect-solar{
+        background-color: #222;
+    }
+
+    .rect-thermal{
+        background-color: #666;
+    }
+
+    .rect-renewable{
+        background-color: #ECC86C;
+    }
+
+    .rect-other{
+        background-color: #E0E0E0;
+    }
+
+</style>
--- src/components/sourcesGraph/sourceGraph.svelte
+++ src/components/sourcesGraph/sourceGraph.svelte
@@ -1,9 +1,42 @@
 <script>
     import { sourcesData } from "../../../data/sources_data";
+    import SourceCountry from "./sourceCountry/sourceCountry.svelte";
 
     console.log(sourcesData);
-</script>
-    <h2> ... SOURCEGRAPH MARKUP ... </h2>
-<style>
 
+    const rbs = Object.keys(sourcesData);
+
+</script>
+
+<div class="title">
+    <h2> Overview of UNDP Solutions </h2>
+    <p>
+        The following chart shows the solutions mapped in different countries. 
+        Colours indicate the type of energy the solution deals with. 
+    </p>
+</div>
+
+<div class="graph-container">
+    {#each rbs as rb }
+        <div class="rb-container {rb}">
+            <h2> {rb} </h2>
+            {#each sourcesData[rb] as country}
+                <SourceCountry country={country} />
+            {/each}
+        </div>
+    {/each}
+</div>
+
+
+<style>
+    .title h2{
+        font-family: "Roboto Condensed";
+        font-size: 36px;
+        margin: 0;
+        margin-bottom: 15px;
+    }
+
+    .title p{
+
+    }
 </style>
